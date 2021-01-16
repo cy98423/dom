df5266a53f53c663e0e75a43761800830a81b532
Cy-Qy <308103222@qq.com>
2021-01-16 09:58:29 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-const div = dom.create(`<div>const div = dom.find('#test>.red')[0] // 获取对应的元素<br>
+const div1 = dom.create(`<div>const div = dom.find('#test>.red')[0] // 获取对应的元素<br>
 dom.style(div, 'color', 'red') // 设置 div.style.color<br>
 <br>
 const divList = dom.find('.red') // 获取多个 div.red 元素<br>
 dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素</div>`);
-console.log(div);
+console.log(div1);
 
-dom.after(test, div);
+dom.after(test, div1);
